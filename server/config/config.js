18457807d79cd26aceb0b84cdb380c234c2c1fb1
jdiego98 <jarguelloq@ucenfotec.ac.cr>
2020-01-12 21:54:05 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,11 +18,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB; 
 
-// if( process.env.NODE_ENV === 'dev'){
-//    urlDB = 'mongodb://localhost/cafe';
-// }else {
-    urlDB = 'mongodb+srv://jdiego:E2KkgAMCrXFc87nZ@cluster0-yvtcg.mongodb.net/test?retryWrites=true&w=majority';
-// }
+if( process.env.NODE_ENV === 'dev'){
+   urlDB = 'mongodb://localhost/cafe';
+}else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
 
